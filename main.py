063d5aeb6399742df88553b936e32c4c303d77cb
ZHU ZIJUN <60607151+ZhuLegend@users.noreply.github.com>
2026-05-05 23:15:22 +0800

    Observability/grpc service metrics (#2)
    * feat(metrics): expose edu admin grpc metrics
    
    * fix(metrics): default edu admin metrics port
    fix(docker): use valid edu metrics port

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import asyncio
+from configparser import Error as ConfigError
 import logging
+import os
 
 import grpc
 
+from _321CQU.tools.gRPCMetrics import GRPCMetricsServerInterceptor, start_metrics_server
 from micro_services_protobuf.edu_admin_center import eac_service_pb2_grpc as eac_grpc
 from _321CQU.tools.gRPCManager import gRPCManager, ServiceEnum
 
 from service import EACServicer
+from utils.tools.configManager import ConfigReader
+
+
+def _get_metrics_port():
+    port = os.getenv("METRICS_PORT")
+    if port:
+        return port
+    try:
+        return ConfigReader().get_config("Metrics", "port")
+    except ConfigError:
+        return "9322"
 
 
 async def serve():
-    port = gRPCManager().get_service_config(ServiceEnum.EduAdminCenter)[1]
+    grpc_manager = gRPCManager(caller="edu_admin_center")
+    port = grpc_manager.get_service_config(ServiceEnum.EduAdminCenter)[1]
 
-    server = grpc.aio.server()
+    metrics_server = await start_metrics_server(_get_metrics_port())
+    server = grpc.aio.server(interceptors=[GRPCMetricsServerInterceptor()])
     eac_grpc.add_EduAdminCenterServicer_to_server(EACServicer(), server)
     server.add_insecure_port('[::]:' + port)
     await server.start()
-    await server.wait_for_termination()
+    try:
+        await server.wait_for_termination()
+    finally:
+        await grpc_manager.close_all()
+        if metrics_server is not None:
+            metrics_server.close()
+            await metrics_server.wait_closed()
 
 
 if __name__ == '__main__':
